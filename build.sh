05b54af1c4cc34cd3bac9883cf228135ff5c0460
Ivan Vnucec <vnucec.ivan@gmail.com>
2025-04-01 20:09:26 +0200

    add working tf-a

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 set -x -e
 
+UBOOT_BUILD_DIR=../u-boot-stm32mp/build-stm32mp157d-bsb
 TFA_DTB=stm32mp157-somic
-TFA_FLAGS="-j8 LOG_LEVEL=LOG_LEVEL_VERBOSE STM32MP_EARLY_CONSOLE=1 STM32MP_USB_PROGRAMMER=1 CROSS_COMPILE=arm-none-eabi- PLAT=stm32mp1 ARCH=aarch32 ARM_ARCH_MAJOR=7 DTB_FILE_NAME=$TFA_DTB.dtb"
+TFA_FLAGS="-j8 STM32MP_USB_PROGRAMMER=1 CROSS_COMPILE=arm-none-eabi- PLAT=stm32mp1 ARCH=aarch32 ARM_ARCH_MAJOR=7 DTB_FILE_NAME=$TFA_DTB.dtb"
+# LOG_LEVEL=LOG_LEVEL_VERBOSE STM32MP_EARLY_CONSOLE=1
 
 rm -rf build
 make $TFA_FLAGS AARCH32_SP=sp_min bl32 dtbs
 make $TFA_FLAGS
-make $TFA_FLAGS AARCH32_SP=sp_min BL33=../u-boot-stm32mp/build/u-boot-nodtb.bin BL33_CFG=../u-boot-stm32mp/build/u-boot.dtb fip
+make $TFA_FLAGS AARCH32_SP=sp_min BL33=$UBOOT_BUILD_DIR/u-boot-nodtb.bin BL33_CFG=$UBOOT_BUILD_DIR/u-boot.dtb fip
